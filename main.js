3e6508fc981fbc5aaaa4f6a1076d5d2a384d9d19
Roman Spiridonov <rom@nspiridonov.ru>
2020-03-03 01:16:56 +0300

    Added game draft

---
+++ main.js
@@ -0,0 +1,30 @@
+let field;
+let currentPlayerEl = document.getElementById("currentPlayer");
+
+let current = "X";
+let steps = 0;
+
+function clearFieldd() {
+  // TODO: очистка поля
+  field = null;
+}
+
+function makeStep() {
+  // TODO: обработка хода игрока
+  // Делаем ход
+  // Проверяем выигрыш, если он возможен
+  // Ничья?
+  // Смена игрока
+}
+
+function checkVictory() {
+  // TODO: функция для проверки победителя
+}
+
+function makeField() {
+  // TODO: генерация пустого поля для игры
+}
+
+function resetGame() {
+  // TODO: перезапуск игры
+}

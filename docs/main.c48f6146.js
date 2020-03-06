508d3512336cfe671b631cf99fb7a90035415507
Roman Spiridonov <rom@nspiridonov.ru>
2020-03-06 11:57:46 +0300

    Добавлен github-fork-ribbon-css. Нужно выполнить npm install

--- docs/main.c48f6146.js
+++ docs/main.c48f6146.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"aRYt":[function(require,module,exports) {
 "use strict";var e=u(require("./Game"));function u(e){return e&&e.__esModule?e:{default:e}}new e.default;
 },{"./Game":"fFAv"}]},{},["aRYt"], null)
-//# sourceMappingURL=/main.c48f6146.js.map
+//# sourceMappingURL=main.c48f6146.js.map
